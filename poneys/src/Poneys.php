827f014245ec3778748529dc1f64d5beb3994789
Your Name <you@example.com>
2016-11-22 14:01:50 +0100

    Initial

--- poneys/src/Poneys.php
+++ poneys/src/Poneys.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   class Poneys {
       private $count = 8;
 
@@ -7,11 +7,23 @@
       }
 
       public function removePoneyFromField($number) {
+        if ($number > $this->count) {
+          throw new Exception("Error Can't have negative amount of poney on the field", 1);
+
+        }
         $this->count -= $number;
+      }
+
+      public function addPoneyToField($number) {
+        $this->count += $number;
       }
 
       public function getNames() {
 
+      }
+
+      public function setCount($number) {
+        $this->count = $number;
       }
   }
 ?>
--- poneys/tests/PoneysTest.php
+++ poneys/tests/PoneysTest.php
@@ -2,15 +2,83 @@
   require_once 'src/Poneys.php';
 
   class PoneysTest extends \PHPUnit_Framework_TestCase {
-    public function test_removePoneyFromField() {
-      // Setup
-      $Poneys = new Poneys();
+    private $PONEY_NUMBER = 8;
+
+    protected function setUp(){
+      $this->PONEY_NUMBER = intval(getenv('PONEY_NUMBER'));
+      $this->poneys = new Poneys();
+      $this->poneys->setCount($this->PONEY_NUMBER);
+    }
+
+
+   /**
+   * @param $number
+   *
+   * @dataProvider validRemovePoneys
+   */
+    public function test_removePoneyFromField($number) {
+      // Action
+      $this->poneys->removePoneyFromField($number);
+
+      // Assert
+      $this->assertEquals($this->PONEY_NUMBER-$number, $this->poneys->getCount());
+    }
+
+    public function test_CanNotHaveNegativePoneyInField() {
+      // expectException
+      $this->expectException(Exception::class);
 
       // Action
-      $Poneys->removePoneyFromField(3);
-      
-      // Assert
-      $this->assertEquals(5, $Poneys->getCount());
+      $this->poneys->removePoneyFromField(100);
     }
+
+
+    public function test_addPoneyToField() {
+      // Action
+      $this->poneys->addPoneyToField(3);
+
+      // Assert
+      $this->assertEquals($this->PONEY_NUMBER+3, $this->poneys->getCount());
+    }
+
+    public function test_getNamesPoneys(){
+      $poneys = $this->createMock(Poneys::class);
+      $poneys->method('getNames')->willReturn(['Stewball','Tata','Yoyo']);
+      $this->assertEquals(['Stewball','Tata','Yoyo'],$poneys->getNames());
+    }
+
+    public function test_setCountPoneys(){
+
+        $this->poneys->setCount(20);
+
+        $this->assertEquals($this->poneys->getCount(), 20);
+    }
+
+
+    public function test_CanNotHaveMoreThan15Poneys(){
+
+        $this->assertLessThanOrEqual(15, $this->poneys->getCount());
+    }
+
+
+    public static function validRemovePoneys()
+    {
+      return [
+        [1],
+        [2],
+        [3],
+        [4],
+        [5],
+        [6],
+        [7],
+        [8]
+      ];
+    }
+
+    protected function tearDown(){
+      unset($this->poneys);
+    }
+
+
   }
  ?>
